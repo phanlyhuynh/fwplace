234cd6dfb4146073dd3e2e9b27b37c2d8e7a93fb
quy <nguyenthequy.ptit@gmail.com>
2018-10-02 14:13:10 +0700

    create migration and model

---
+++ app/Models/User.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'status',
+
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $guarded = ['id'];
+
+    public function program()
+    {
+        return $this->belongsTo('App\Models\Program');
+    }
+
+    public function workspace()
+    {
+        return $this->belongsTo('App\Models\WorkSpace');
+    }
+
+    public function position()
+    {
+        return $this->belongsTo('App\Models\Position');
+    }
+
+    public function workschedules()
+    {
+        return $this->hasMany('App\Models\WorkSchedule', 'user_id');
+    }
+
+}
---
+++ app/Models/WorkSchedule.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WorkSchedule extends Model
+{
+    protected $guarded = ['id'];
+
+    public function calendars()
+    {
+        return $this->hasMany('App\Models\Calendar', 'work_schedule_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+}
